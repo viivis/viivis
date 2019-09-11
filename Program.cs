0d7c920dfa2c7922e9b62742ed38f733ab32edba
Viivi <viivi@example.com>
2019-09-11 12:43:04 +0300

    git commit -m "failid"
    git push origin master
    git push origin master
    
    git push -u origin master

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeineTund
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
